92a436ae0e7db314c45040014ce97477b48c2b44
Richard Wasonga <rowwasonga@gmail.com>
2025-05-15 14:05:44 +0300

    complete lab

--- lib/owner_pet.py
+++ lib/owner_pet.py
@@ -1,5 +1,27 @@
 class Pet:
-    pass
+    all = []
+    PET_TYPES = ["dog", "cat", "rodent", "bird", "reptile", "exotic"]
+    def __init__(self,name,pet_type,owner=None):
+        if pet_type not in Pet.PET_TYPES:
+            raise ValueError ("Pet_type must be in the PET_TYPES list")
+        self.name = name
+        self.pet_type = pet_type
+        self.owner = owner
+        Pet.all.append(self)
 
 class Owner:
-    pass
+    def __init__(self,name):
+        self.name = name
+
+    def pets(self):
+        return [pet for pet in Pet.all if pet.owner == self ]
+
+    def add_pet(self,pet):
+        if not isinstance (pet,Pet):
+            raise TypeError("Pet does not exist in Pet class")
+
+        pet.owner = self
+
+    def get_sorted_pets(self):
+        sorted_list = sorted(self.pets(), key=lambda pet: pet.name)
+        return sorted_list
